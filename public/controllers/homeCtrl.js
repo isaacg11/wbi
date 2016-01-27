7fa8f10c3f006e2cd12064250aa23408ffe05aa3
isaacg11 <isaacgreywebdeveloper@gmail.com>
2016-01-26 22:42:18 -0800

    initial commit

---
+++ public/controllers/homeCtrl.js
@@ -0,0 +1,29 @@
+ (function() {
+  'use strict';
+  angular.module('stamplay')
+  .controller('homeCtrl', homeCtrl);
+  homeCtrl.$inject = ['homeFactory', '$state',"$http","$scope", "$stamplay","$location","$anchorScroll"];
+
+  function homeCtrl(homeFactory, $state, $http, $scope, $stamplay, $location, $anchorScroll, duParallax){
+
+    $scope.words = "Hello world";
+
+  	$scope.goToContact = function() {
+      $location.hash('contact');
+      $anchorScroll();
+  	};
+  	
+      
+  	$scope.goToServices = function(){
+  		$location.hash('services');
+      	$anchorScroll();
+  	};
+
+  	$scope.goToReviews = function(){
+  		$location.hash('reviews');
+      	$anchorScroll();
+  	};
+
+  //CLOSING BRACKETS
+  }
+  })();  
